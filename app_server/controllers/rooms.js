769f82ffaf8544fe77cbfe49af0b2cbe5b154f14
mipo2 <michael.pollak2@snhu.edu>
2023-03-15 22:52:11 -0500

    module 3

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+
+var bookableRooms = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf-8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', { title: 'Travlr Getaways', path: "rooms" });
+    res.render('rooms', { title: 'Travlr Getaways', path: "rooms", bookableRooms });
 };
 
 module.exports = {
