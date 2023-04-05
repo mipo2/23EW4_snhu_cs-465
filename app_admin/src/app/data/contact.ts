6aff8c04eb512d9c048a7dce96cd9ecca0d2ffd2
mipo2 <michael.pollak2@snhu.edu>
2023-04-05 18:10:57 -0500

    setup angular and added trip listing component

---
+++ app_admin/src/app/data/contact.ts
@@ -0,0 +1,6 @@
+export const contacts = {
+    "name": "Travlr Getaways",
+    "address": "123 Lorem Ipsum Cove, Sed Ut City, LI 12345",
+    "phoneNumber": "1-800-999-9999",
+    "faxNumber": "1-800-111-1111"
+}
---
+++ app_admin/src/app/data/rooms.ts
@@ -0,0 +1,17 @@
+export const rooms = [
+    {
+        "name": "First Class Room",
+        "image": "first-class.jpg",
+        "description": "<p>First Class Room cras dui sapien, feugiat vitae tristique ut, lobortis tempor orci. Donec pulvinar sagittis metus ut tristique. Pellentes que habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas idios.</p><span class=\"rate\">Rate: 220 / Day</span>"
+    },
+    {
+        "name": "Deluxe Room",
+        "image": "deluxe.jpg",
+        "description": "<p>Deluxe Room sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis.</p><span class=\"rate\">Rate: 150 / Day</span>"
+    },
+    {
+        "name": "Suite Room",
+        "image": "suite.jpg",
+        "description": "<p>Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis.</p><span class=\"rate\">Rate: 180 / Day</span>"
+    }
+]
